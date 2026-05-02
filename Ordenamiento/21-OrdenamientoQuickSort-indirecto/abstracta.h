90a2a3095a66713f18f1feb82897d623dcc8aec5
Al-Mejia-0920 <aldo.mejia0920@gmail.com>
2026-05-02 12:04:45 -0600

    Add files via upload

---
+++ Ordenamiento/21-OrdenamientoQuickSort-indirecto/abstracta.h
@@ -0,0 +1,14 @@
+#ifndef ABSTRACTA_H_INCLUDED
+#define ABSTRACTA_H_INCLUDED
+
+class dadlista {
+public:
+    int arreglo[20];   
+    int* punteros[20]; 
+    int i;            
+    dadlista() {
+        i = 0;
+    }
+};
+
+#endif
